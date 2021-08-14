9abdb540d10c5ac6e40e2ed306b16b4124b66430
joseluisgomezcecena <joseluisgomezcecegna@gmail.com>
2021-08-13 17:39:53 -0700

    contact page and front page integrated with acf

--- contact-page.php
+++ contact-page.php
@@ -7,27 +7,17 @@ get_header();
 ?>
 
 
-<section style="background: url('<?php echo the_field('header_image') ?>'); width:100%;height:500px;background-repeat: no-repeat;background-position: center;background-size:center;" >
+<section style="background: url('<?php echo the_field('header_image') ?>'); width:100%;height:500px;background-repeat: no-repeat;background-position: center;background-size:center;background-attachment:fixed;" >
     <div class="overlaybg">
         <div class="container-fluid overlaycontent">
             <div class="row">
                 
                 <div class="col-md-10 offset-md-1">
-
                     <div class="row">
-
-                       
                         <div class="col-lg-8 offset-lg-2 text-center">
-                       
-                                
-                            
-                            <p><a href="#" class="btn-alseide btn-alseide-4 btn">Contacto</a></p>
+                            <h2 class="fs-1"><?php echo the_field('header_image_text'); ?></h2>
                         </div>
-                        
-
                     </div>
-                    
-
                 </div>
                 
             </div>
@@ -38,15 +28,62 @@ get_header();
 
 <section class="page_contents">
     <div class="container">
+    
         <div class="row">
-            
-            <div class="col-lg-12 col-md-12">
+            <div class="col-lg-12 px-4 py-5 p-lg-5 all-text-white">
+                <div class="h-100">
+                    <div class="title text-center p-0">
+                        <br><br>
+                        <span class="pre-title"><?php echo the_field('call_to_action_text_small'); ?></span>
+                        <h2><?php echo the_field('call_to_action_tiltle'); ?></h2>
+                        <p><?php echo the_field('call_to_action_text'); ?></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+        <div class="row">
+            <div class="col-lg-6 col-md-6 pb-5">
+                <article class="main_content">
+
+                    <div class="card shadow-sm">
+                        <div class="card-body">
+                            <h5 class="card-title">Información de Contacto.</h5>
+                            <p class="card-text">
+                                <?php 
+                                $phone = get_field('phone'); 
+                                if($phone):
+                                ?>
+                                Telefono: <?php the_field('phone'); ?> 
+                                <?php 
+                                endif;
+                                ?>
+                            </p>
+                            <p class="card-text">
+                            <?php 
+                                $email = get_field('email'); 
+                                if($email):
+                                ?>
+                                Email: <?php the_field('email'); ?> 
+                                <?php 
+                                endif;
+                                ?>
+                            </p>
+                            <a href="tel:+526862594318" class="btn btn-primary">Llamanos!</a>
+                        </div>
+                    </div>
+
+                </article>
+            </div>
+            <div class="col-lg-6 col-md-6 pb-5">
                 <article class="main_content">
                     <?php the_content() ?>
                 </article>
             </div>
-
         </div>
+
+
     </div>
 </section>
 
--- front-page copy.php
+++ front-page copy.php
@@ -29,13 +29,29 @@ get_header();
 
                     <div class="row">
 
-                       
+                        <!--
+                        <div class="col-lg-6">
+                            <h5>Alseide</h5>
+                            <p>Paisajismo Consciente</p>
+                            <p><a href="#" class="btn btn-dark btn-large">Learn more »</a></p>
+                        </div>
+                        -->
                         <div class="col-lg-8 offset-lg-2 text-center">
-                       
+                            <!--
+                            <img src="<?php echo get_template_directory_uri() ?>/images/logo.png" width="50%" class="img-fluid" alt="Alseide">
+                            -->
+                            <!--
+                            <div class="banner_text">
+                                <h1 class="fs-5">Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit cum veniam porro facere voluptas exercitationem atque, recusandae odio blanditiis nisi eos eaque quisquam doloremque.</h1>
+                            </div>
+                            -->
+
                             <?php if ( is_active_sidebar( 'front_page_banner_text' ) ) : ?>
                                     <?php dynamic_sidebar( 'front_page_banner_text' ); ?>
                             <?php endif; ?>
-     
+
+
+                            
                             <p><a href="#" class="btn-alseide btn-alseide-4 btn">Contacto</a></p>
                         </div>
                         
@@ -51,63 +67,61 @@ get_header();
 </section>
 
 
-
-
-<?php 
-$features = get_field('display_servicios_op');
-if($features == 'yes'):
-?>
 <section class="features pt-5 pb-5">
     <div class="container">
         <div class="row">
             
             <div class="col-lg-3  col-md-6">
-                <div style="  border-top: 4px solid #03991a;" class="card shadow h-100">
-                    <img src="<?php the_field('1_servicio_imagen'); ?>" class="card-img-top" alt="<?php the_field('1_servicio_titulo'); ?>">
+                <div style="  border-top: 4px solid #03991a;" class="card shadow">
+                    <img src="<?php echo get_template_directory_uri() ?>/images/huertos.png" class="card-img-top" alt="...">
                     <div class="card-body">
-                        <h5 class="card-title"><?php the_field('1_servicio_titulo'); ?></h5>
-                        <p class="card-text"><?php the_field('1_servicio_texto'); ?></p>
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
                     </div>
                 </div>
             </div>
 
-            
             <div class="col-lg-3  col-md-6">
-                <div style="  border-top: 4px solid #03991a;" class="card shadow h-100">
-                    <img src="<?php the_field('2_servicio_imagen'); ?>" class="card-img-top" alt="<?php the_field('2_servicio_titulo'); ?>">
+                <div style="  border-top: 4px solid #03991a;" class="card shadow">
+                    <img src="<?php echo get_template_directory_uri() ?>/images/riego.png" class="card-img-top" alt="...">
                     <div class="card-body">
-                        <h5 class="card-title"><?php the_field('2_servicio_titulo'); ?></h5>
-                        <p class="card-text"><?php the_field('2_servicio_texto'); ?></p>
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
                     </div>
                 </div>
             </div>
 
-            
             <div class="col-lg-3  col-md-6">
-                <div style="  border-top: 4px solid #03991a;" class="card shadow h-100">
-                    <img src="<?php the_field('3_servicio_imagen'); ?>" class="card-img-top" alt="<?php the_field('3_servicio_titulo'); ?>">
+                <div style="  border-top: 4px solid #03991a;" class="card shadow">
+                    <img src="<?php echo get_template_directory_uri() ?>/images/perm.png" class="card-img-top" alt="...">
                     <div class="card-body">
-                        <h5 class="card-title"><?php the_field('3_servicio_titulo'); ?></h5>
-                        <p class="card-text"><?php the_field('3_servicio_texto'); ?></p>
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
                     </div>
                 </div>
             </div>
 
-            
             <div class="col-lg-3  col-md-6">
-                <div style="  border-top: 4px solid #03991a;" class="card shadow h-100">
-                    <img src="<?php the_field('4_servicio_imagen'); ?>" class="card-img-top" alt="<?php the_field('4_servicio_titulo'); ?>">
+                <div style="  border-top: 4px solid #03991a;" class="card shadow">
+                    <img src="<?php echo get_template_directory_uri() ?>/images/florafauna.png" class="card-img-top" alt="...">
                     <div class="card-body">
-                        <h5 class="card-title"><?php the_field('4_servicio_titulo'); ?></h5>
-                        <p class="card-text"><?php the_field('4_servicio_texto'); ?></p>
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
                     </div>
                 </div>
             </div>
+
+           
+
+
             
+
+
+
         </div>
     </div>
 </section>
-<?php endif; ?>
+
 
 
 
