9ba3fb7ca0c5c8b0f8d6c22ef475144ff8bc85cc
joseluisgomezcecena <joseluisgomezcecegna@gmail.com>
2021-08-16 17:49:01 -0700

    added 404 page

---
+++ 404.php
@@ -0,0 +1,21 @@
+<?php get_header(); ?>
+
+<section class="bgimage404">
+    <div class="overlaybg404">
+        <div class="container-fluid overlaycontent404">
+            <div class="row">
+                <div class="col-md-10 offset-md-1">
+                    <div class="row">
+                        <div class="col-lg-8 offset-lg-2 text-center">
+                            <h1 class="fourofour">404</h1> 
+                            <h1 class="fourofour">Parece un desierto por aqui</h1>
+                            <p class="fourofour-text">No podemos encontrar la pagina :(</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<?php get_footer(); ?>
